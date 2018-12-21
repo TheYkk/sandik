6462a3387c061af1ad568e89f923eb9ea5bd4c1a
Yusuf Kaan Karakaya <yusufkaan142@gmail.com>
2018-12-21 20:11:44 +0300

    confs

--- init.js
+++ init.js
@@ -6,11 +6,7 @@ var log = console.log;
 db.configs.findOne({token:{ $exists: true }},function (err,doc) { 
     if (doc){
         log(chalk.red('Alredy Setup!'));
-        db.configs.update({ token: '0.rn4ymmoia0eas3p5sxu5ck' }, { $inc: { "data.satellites":Math.random(), "data.red": true } }, { upsert: true }, function (err, numReplaced) {
-            // numReplaced = 3
-            // Field 'system' on Mars, Earth, Jupiter now has value 'solar system'
-          });
-        //process.exit();
+        process.exit();
     }else {
         //Kurulum
        db.configs.insert({token : utils.rand()}, function (err, newDoc) { // Callback is optional
